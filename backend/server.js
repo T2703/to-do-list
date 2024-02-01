2cde8242a8e8eef395c9ecb8f85d10f2a74e7613
Tristan <ricemaking69@gmail.com>
2024-02-01 11:29:07 -0600

    added in part of the mongo stuff

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const app = express()
+const mongoose = require('mongoose');
+const app = express();
+const uri = 'mongodb+srv://<T0207>:<s7O7JXnqNs7nOzCJ>@clustertodo.bwhgqov.mongodb.net/?retryWrites=true&w=majority';
+
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    res.json({"users": ["userOne", "userTwo", "userThree"]});
 })
+
+connect();
 
 app.listen(5000, () => {console.log("Server started on port 5000")});
